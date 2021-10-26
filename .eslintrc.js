3f7b7f4fc22e1064f4971102a54b82ecbad3c42f
Vasil Vanchuk <v.vanchuk@tut.by>
2021-10-26 21:23:07 +0300

    Setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', {
+      ignoreComments: true,
+    }],
+  },
+  plugins: ['jest'],
 };
